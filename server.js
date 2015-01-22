2d6071f9a3ccd1a9588317c9cebb13a29168e491
Tina Fallahi <fallahi.tina@gmail.com>
2015-01-22 15:00:36 +0000

    Added initial backend.

---
+++ server.js
@@ -0,0 +1,26 @@
+var pg = require('pg');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+//app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (request, response) {
+  response.send("Welcome to Smellscape!");
+});
+
+app.get('/db', function(request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+      { console.error(err); response.send("Error " + err); }
+      else 
+      { response.send(result.rows); }
+    });
+  });
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
